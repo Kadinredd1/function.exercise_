710e7ed34da2521110219e253184d7abf2e4f5ad
Kadinredd1 <67976031+Kadinredd1@users.noreply.github.com>
2020-07-21 14:06:34 -0500

    Add files via upload

---
+++ function.exercise.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+func addition(num1: Int, num2: Int) -> Int {
+  let problem = num1 + num2
+  return problem
+}
+print(addition(num1: 12,num2: 23))
